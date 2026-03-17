92c108095c4cbab20669f6fd0c2a0886a14eb0de
Justin Schulz <Jschulz8138@conestogac.on.ca>
2026-03-17 14:13:18 -0400

    Edited RoboDad CMakeLists.txt and main.cpp for basic crow set up

--- RoboDad/src/main.cpp
+++ RoboDad/src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#define ASIO_STANDALONE
+#include "asio.hpp"
+#include "crow/crow_all.h"
 
 int main() {
-	std::cout << "Hello, RoboDad!" << std::endl;
-	return 0;
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([]() {
+        return "<h1>Hello, Crow + ASIO is working!</h1>";
+        });
+
+    std::cout << "Server running on http://127.0.0.1:18080\n";
+
+    app.bindaddr("127.0.0.1").port(18080).multithreaded().run();
 }
